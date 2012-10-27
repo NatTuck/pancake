3802a1440a542c50536176f48a9b734044d415dc
Nat Tuck <nat@ferrus.net>
2012-10-27 16:40:32 -0400

    Can do .cl -> Module* -> llvm asm.

--- scripts/llvm-libs.pl
+++ scripts/llvm-libs.pl
@@ -2,9 +2,6 @@
 use 5.10.0;
 use warnings FATAL => 'all';
 
-say `llvm-config --libs`;
-exit 0;
-
 my @libs  = split /\s+/, `llvm-config --libfiles`;
 my @flags = ();
 
@@ -16,28 +13,5 @@ for my $lib (@libs) {
     }
 }
 
-@flags = ();
-
-while (<DATA>) {
-    for my $flag (split /\s+/, $_) {
-	push @flags, $flag;
-    }
-}
-
 say join(" ", @flags);
-
-__DATA__
--lclangFrontend -lclangSerialization -lclangDriver -lclangCodeGen 
--lclangParse -lclangSema -lclangStaticAnalyzerFrontend 
--lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore 
--lclangAnalysis -lclangRewrite -lclangEdit -lclangAST 
--lclangLex -lclangBasic -lLLVMInstrumentation -lLLVMAsmParser 
--lLLVMLinker -lLLVMArchive -lLLVMipo -lLLVMVectorize -lLLVMBitWriter 
--lLLVMBitReader -lLLVMX86CodeGen -lLLVMX86Desc -lLLVMX86Info 
--lLLVMX86AsmPrinter -lLLVMX86Utils -lLLVMSelectionDAG 
--lLLVMAsmPrinter -lLLVMMCParser -lLLVMInterpreter -lLLVMJIT 
--lLLVMRuntimeDyld -lLLVMExecutionEngine -lLLVMCodeGen 
--lLLVMScalarOpts -lLLVMInstCombine -lLLVMTransformUtils 
--lLLVMipa -lLLVMAnalysis -lLLVMTarget -lLLVMMC -lLLVMObject 
--lLLVMCore -lLLVMSupport   -lpthread -ldl -lm 
 
